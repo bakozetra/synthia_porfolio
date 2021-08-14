d41cb0bd541e5cc78e59220ce4ecee73bd8f51f9
Synthia Hanitriniaina <Bakozetra>
2021-08-14 09:11:40 +0300

    Feat(portfolio) Added my personal information and some projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'synthia.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
@@ -28,30 +28,30 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'country-q.png',
+    title: 'Country quiz',
+    info: "This project  is based on sCSS , SASS , and REACT. I started from scratch. It generates an API of the country's name, capital of the country and fl",
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://country-quiz-synthia.netlify.app/',
+    repo: 'https://github.com/bakozetra/country-quiz', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'birthday-app.png',
+    title: 'Birthday App',
+    info: 'This project is built with CSS and vanilla javascript. It generates an API of people’s birthday dates. This app allows you to add a birthday date  and remove it from this app.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://synthia-birthday-app.netlify.app/',
+    repo: 'https://github.com/bakozetra/birthday-app', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'photograph.png',
+    title: 'Photograph',
+    info: 'I built this project from scratch. It is built with CSS , SASS , and HTMl5.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://front-end-finals.netlify.app/',
+    repo: 'https://github.com/bakozetra/front-end-finals', // if no repo, the button will not show up
   },
 ];
 
@@ -65,30 +65,30 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    // {
+    //   id: nanoid(),
+    //   name: 'twitter',
+    //   url: '',
+    // },
+    // {
+    //   id: nanoid(),
+    //   name: 'codepen',
+    //   url: '',
+    // },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/synthia-hanitriniaina-1ab69a1a5/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/bakozetra',
     },
   ],
 };
 
 // Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
-};
+// export const githubButtons = {
+//   isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+// };
